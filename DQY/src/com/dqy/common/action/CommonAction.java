3c464bacc63d0d72306c93dd81716f69c46dd840
zhenjiaWang <zhenjiaWang@gmail.com>
2013-12-24 14:00:28 +0800

    123de

--- DQY/src/com/dqy/common/action/CommonAction.java
+++ DQY/src/com/dqy/common/action/CommonAction.java
@@ -334,6 +334,7 @@ public class CommonAction extends BaseAction {
                     if (userList != null && !userList.isEmpty()) {
                         boolean flag=true;
                         for (HrUser hrUser : userList) {
+                            flag=true;
                             if(StringUtils.isNotBlank(notMe)){
                                 if(hrUser.getId().equals(userInfo.getUserId())){
                                     flag=false;
